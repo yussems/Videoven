5d6289a1d457762003c2462cf020e91121331bfd
yussems <yussems@gmail.com>
2022-01-28 22:28:58 +0300

    finished phone content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Content from './component/Content';
 import Header from './component/Header';
+import Interaction from './component/Interaction';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <Interaction />
     </div>
   );
 }

f1c42482a6f6761619f28493ed6ca14d1d5a245f
yussems <yussems@gmail.com>
2022-01-28 23:43:14 +0300

    change to responsive design

--- src/component/Interaction.js
+++ src/component/Interaction.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./interaction.css";
+import RightSide from "./RightSide";
 function Interaction() {
   return (
     <div className="interaction-container">
-      <div className="inner-interaction"></div>
+      <div className="inner-interaction">
+        <div className="section-start">
+
+        </div>
+      </div>
     </div>
   );
 }

a4faf3f67b2b909ea49e7de6f5efa7275c7e6153
yussems <yussems@gmail.com>
2022-01-28 15:13:25 +0300

    added content.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Content from './component/Content';
 import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/component/Content.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./content.css";
+function Content() {
+  return (
+    <main>
+      <div className="left-side">
+        <div className="vektor-top"></div>
+        <div>
+          <h1>İşinizi Güçlendirecek Videolar</h1>
+        </div>
+      </div>
+
+      {/* right side */}
+    </main>
+  );
+}
+
+export default Content;

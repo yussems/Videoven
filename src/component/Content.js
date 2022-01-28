29d582c25ac38ad18f059f069b2fa4efe63765f9
yussems <yussems@gmail.com>
2022-01-28 16:00:29 +0300

    added content font-family

--- src/component/Content.js
+++ src/component/Content.js
@@ -5,9 +5,24 @@ function Content() {
     <main>
       <div className="left-side">
         <div className="vektor-top"></div>
-        <div>
+        <div className="ball"></div>
+        
+        <div className="content" >
           <h1>İşinizi Güçlendirecek Videolar</h1>
+          <h2>Artık İnteraktif</h2>
+          <p className="paraf">
+            Videoven ile hedeflediğiniz kitleye videolar aracılığıyla ulaşın.
+            Tek hamlede binlerce kullanıcıdan zahmetsizce bilgi alın.
+            Maliyetlerinizi azaltın, müşteri memnuniyetini ve satışlarınızı
+            artırın. Anlatan, seçenek sunan, bilgi toplayan videolarla çevrimiçi
+            iletişimin yeni seviyesine adım atın.
+          </p>
+          <button className="content-button">Hemen Deneyin</button>
+          
         </div>
+        <div className="vektor-container">
+            <div className="vektor-bottom"></div>
+          </div>
       </div>
 
       {/* right side */}

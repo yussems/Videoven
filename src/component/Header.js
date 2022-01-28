1fce5a28a7e4a0c452d31901ae2279e196a29b0d
yussems <yussems@gmail.com>
2022-01-28 14:44:55 +0300

    added responosive rules

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,29 +1,31 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 function Header() {
   return (
     <div className="Header">
-      <p className="logo">Videoven</p>
-      <ul>
-        <li>
-          <a href="#">Bize Katıl</a>
-        </li>
-        <li>
-          <a href="#">Stratejiler</a>
-        </li>
-        <li>
-          <a href="#">Plan</a>
-        </li>
-        <li>
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <a href="#">Yardım</a>
-        </li>
-      </ul>
+      <div className="header-logo">
+        <p className="logo">Videoven</p>
+      </div>
+        <ul>
+          <li>
+            <a href="#">Bize Katıl</a>
+          </li>
+          <li>
+            <a href="#">Stratejiler</a>
+          </li>
+          <li>
+            <a href="#">Plan</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Yardım</a>
+          </li>
+        </ul>
       <div className="btn-group">
-          <button className="outline">Oturum Aç</button>
-          <button className="solid">Ücretsiz Dene</button>
+        <button className="outline">Oturum Aç</button>
+        <button className="solid">Ücretsiz Dene</button>
       </div>
     </div>
   );
